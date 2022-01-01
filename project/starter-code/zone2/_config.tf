36a9766b4e01449c63661f2960830dee03a2c756
lekhraj.singh <lekhraj.singh@grabpay.com>
2022-01-01 20:36:38 +0530

    added screenshots and some code modification

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,17 +1,23 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-lekhraj-zone2"
+     bucket = "udacity-tf-lekhraj-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
      profile = "udacity"
    }
  }
 
- provider "aws" {
-   region = "us-west-1"
-   profile = "udacity"
+  provider "aws" {
+   region = "us-east-2"
    
    default_tags {
      tags = local.tags
    }
+   profile = "udacity"
  }
+
+ provider "aws" {
+  alias  = "usw1"
+  region = "us-west-1"
+  profile = "udacity"
+}
